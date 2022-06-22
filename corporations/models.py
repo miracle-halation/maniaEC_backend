6c8637eff3ce5a1117bbee7526939219dfa93746
miracle-halation <miraclehalation.9710@gmail.com>
2022-06-22 18:05:23 +0900

    corporationsにmodelsを作成、migrate実行

--- corporations/models.py
+++ corporations/models.py
@@ -1,3 +1,19 @@
+import email
 from django.db import models
 
 # Create your models here.
+class Company(models.Model):
+	name = models.CharField(max_length=255)
+
+	def __str__(self):
+			return self.name
+
+class Employee(models.Model):
+	name = models.CharField(max_length=255)
+	email = models.EmailField(max_length=255)
+	password = models.CharField(max_length=255)
+	company = models.ForeignKey(Company, on_delete=models.CASCADE)
+	manager = models.BooleanField()
+
+	def __str__(self):
+		return self.name
