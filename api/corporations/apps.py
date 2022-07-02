29c555c037695f0612c2ba7f085c6000d93fc04c
miracle-halation <miraclehalation.9710@gmail.com>
2022-07-02 12:16:52 +0900

    追加で修正が必要だった部分を変更

--- api/corporations/apps.py
+++ api/corporations/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class CorporationsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'corporations'
+    name = 'api.corporations'
